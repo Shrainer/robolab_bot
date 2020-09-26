b33d79cf253255e8faa73615b44733f41c8da0e9
Shrainer <41576544+Shrainer@users.noreply.github.com>
2020-09-26 19:10:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,16 @@ def help(message):
 	
 if __name__ == '__main__':	
 	dict_of_funcs = {}
+	description_position = 0
 	globals_copy = globals().copy()
 	for key in globals_copy:
 		if(isinstance(globals_copy[key], FunctionType)):
         	dict_of_funcs[key] = globals_copy[key].__defaults__ #составляем словарь функций и их описаний
 	help_message = ""
 	for func in dict_of_funcs:
-		help_message+= f"{func} —— {dict_of_funcs[func]}'\n'" #создаём сообщение для команды /help со всеми командами и их кратким описанием
-												  
+		if(func != 'help'):
+			help_message+= f"{func} —— {dict_of_funcs[func][description_position]}'\n'"
+		#создаём сообщение для команды /help со всеми командами и их кратким описанием
 	@bot.message_handler(content_types = ['text'])
 	def handler(message):
 		function_name = message.text[1:]
@@ -31,7 +33,8 @@ if __name__ == '__main__':
 			function = globals_copy[function_name]
 			function(message)
 		except KeyError:
-			bot.send_message(message.chat.id, "Неизвестная команда. Напишите команду '/help' для списка команд") #если пользователь написал существующую функцию, выполняем её
+			bot.send_message(message.chat.id, "Неизвестная команда. Напишите команду '/help' для списка команд") 
+			#если пользователь написал существующую функцию, выполняем её
 	
 	bot.infinity_polling(True)
 												  
