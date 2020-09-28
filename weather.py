12ca73b27b4449cc0921f490fe878e09e49a31f1
Gigond_SuperGnom <41576544+Shrainer@users.noreply.github.com>
2020-09-28 18:54:27 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,10 +3,9 @@ import nasapy, urllib
 
 api = nasapy.Nasa(key = 'key')
 
-def weather():
+def get_weather():
     data = api.mars_weather()
     tt = json.loads(data.text)
     for i in tt:
         return tt[i]["AT"], tt[i]["HWS"]
-
     print(data)
