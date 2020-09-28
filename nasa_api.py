60d9db1709673960e10cbde9e48a354a7a31b6c8
Gigond_SuperGnom <41576544+Shrainer@users.noreply.github.com>
2020-09-28 18:24:51 +0300

    Update nasa_api.py

--- nasa_api.py
+++ nasa_api.py
@@ -1,5 +1,10 @@
-import nasapy
+import nasapy, urllib
 
-api = Nasa(key = 'key')
+api = nasapy.Nasa(key = 'key')
 
 picture = api.picture_of_the_day()
+text = picture['explanation']
+url_to_photo = picture['url']
+with photo as open('picture.jpg', 'wb'):
+	photo.write(urllib.request.urlopen(url).read())
+	photo.close()
