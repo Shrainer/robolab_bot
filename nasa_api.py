0683852eff19954f95afd9eb56858fb47a48b8ef
Gigond_SuperGnom <41576544+Shrainer@users.noreply.github.com>
2020-09-28 19:52:38 +0300

    Update nasa_api.py

--- nasa_api.py
+++ nasa_api.py
@@ -1,26 +1,29 @@
 import nasapy, urllib
-from time import strftime, gmtime 
+from time import strftime, gmtime
 
 api = nasapy.Nasa(key = 'key')
+flag = False
 
 def get_photo():
-	picture = api.picture_of_the_day()
-	date_of_photo = picture['date']
-	text = picture['explanation']
-	today_day = strftime("%Y-%m-%d", gmtime())
-	if(date_of_photo != today_day):
-		with open('picture.jpg', 'wb') as photo:
-			url_to_photo = picture['url']
-			photo.write(urllib.request.urlopen(url).read())
-			photo.close()
-	with open('picture.jpg', rb) as photo:
-		return text, photo
+    global flag
+    picture = api.picture_of_the_day()
+    date_of_photo = picture['date']
+    text = picture['explanation']
+    today_day = strftime("%Y-%m-%d", gmtime())
+    if(date_of_photo != today_day or flag == False):
+        flag = True
+        with open('picture.jpg', 'wb') as photo:
+            url_to_photo = picture['url']
+            photo.write(urllib.request.urlopen(url_to_photo).read())
+            photo.close()
+    return text
 
 def get_weather():
-	data = api.mars_weather()
-	def	get_data(data):
-		for i in data:
-			return data[i]["AT"], data[i]["HWS"]
-	result = get_data(data)
-	return result[0]['av']
+    data = api.mars_weather()
+    def	get_data(data):
+        for i in data:
+            return data[i]["AT"], data[i]["HWS"]
+    result = get_data(data)
+    return result[0]['av']
+
 #Tabs – 4 хватит
