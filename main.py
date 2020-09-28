7d08405f6d730b798efb110137a5e256a03d3bca
Gigond_SuperGnom <41576544+Shrainer@users.noreply.github.com>
2020-09-28 19:52:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import telebot
 from types import FunctionType
-from nasa_api import get_photo, get_wheather
 
 token = 'token'
-bot = telebot.Telebot(token)
+bot = telebot.TeleBot(token)
 
-def start(messsage, description = "Приветствие"):
+def start(message, description = "Приветствие"):
 	bot.send_message(message.chat.id, "Привет, спасибо, что написал мне)))")
 
 def weather(message, description = "Получить данные о погоде на Марсе, которые мы сами не знаем, как обрабатывать"):
-	average_temperature = get_wheather()
+	average_temperature = get_weather()
 	bot.send_message(message.chat.id, f"Сегогдняшняя средняя температура на Марсе: {average_temperature}C")
-	
+
 def photo(message, description = "Получить свежую фотку с Марса, но не в hd :/"):
-	explanation, picture = get_photo()
-	bot.send_photo(message.chat.id, picture)
+	explanation = get_photo()
+	with open('picture.jpg', 'rb') as picture:
+		bot.send_photo(message.chat.id, picture)
+		picture.close()
 	bot.send_message(message.chat.id, explanation)
 
 def help(message):
 	global help_message
 	bot.send_message(message.chat.id, help_message)
-	
-if __name__ == '__main__':	
+
+if __name__ == '__main__':
 	dict_of_funcs = {}
 	description_position = 0
 	globals_copy = globals().copy()
 	for key in globals_copy:
 		if(isinstance(globals_copy[key], FunctionType)):
-        	dict_of_funcs[key] = globals_copy[key].__defaults__ #составляем словарь функций и их описаний
+			dict_of_funcs[key] = globals_copy[key].__defaults__ #Создаём словарь функций и их описаний
 	help_message = ""
 	for func in dict_of_funcs:
 		if(func != 'help'):
-			help_message+= f"{func} —— {dict_of_funcs[func][description_position]}\n"
-		#создаём сообщение для команды /help со всеми командами и их кратким описанием
+			help_message+= f"{func} —— {dict_of_funcs[func][description_position]}\n" #Создаём сообщение для команды '/help'
+	from nasa_api import get_photo, get_weather
 	@bot.message_handler(content_types = ['text'])
 	def handler(message):
 		function_name = message.text[1:]
@@ -40,8 +41,7 @@ if __name__ == '__main__':
 			function = globals_copy[function_name]
 			function(message)
 		except KeyError:
-			bot.send_message(message.chat.id, "Неизвестная команда. Напишите команду '/help' для списка команд") 
-			#если пользователь написал существующую функцию, выполняем её
-	
+			bot.send_message(message.chat.id, "Неизвестная команда. Напишите команду '/help' для списка команд")
+	#Если пользователь ввёл существующую команду, то выполняем её
 	bot.infinity_polling(True)
 												  
