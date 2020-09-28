1d9eeb4de8eb4359de74345f306fa150ffb841ad
Gigond_SuperGnom <41576544+Shrainer@users.noreply.github.com>
2020-09-28 18:52:15 +0300

    Update nasa_api.py

--- nasa_api.py
+++ nasa_api.py
@@ -1,11 +1,17 @@
 import nasapy, urllib
+from time import strftime, gmtime 
 
 api = nasapy.Nasa(key = 'key')
 
-picture = api.picture_of_the_day()
-text = picture['explanation']
-url_to_photo = picture['url']
-date_of_photo = picture['date']
-with photo as open('picture.jpg', 'wb'):
-	photo.write(urllib.request.urlopen(url).read())
-	photo.close()
+def get_photo():
+	picture = api.picture_of_the_day()
+	date_of_photo = picture['date']
+	today_day = strftime("%Y-%m-%d", gmtime())
+	if(date_of_photo != today_day):
+		with open('picture.jpg', 'wb') as photo:
+			text = picture['explanation']
+			url_to_photo = picture['url']
+			photo.write(urllib.request.urlopen(url).read())
+			photo.close()
+	with open('picture.jpg', rb) as photo:
+		return text, photo
