b28818f34e89bf7abe17182170d1a64f55bf4e6d
Shrainer <nikita@shrainer.me>
2020-10-06 17:05:08 +0300

    f

--- nasa_api.py
+++ nasa_api.py
@@ -1,5 +1,5 @@
 import nasapy
-
+from time import strftime, gmtime
 api = nasapy.Nasa(key = 'key')
 
 def get_media():
@@ -11,4 +11,13 @@ def get_weather():
         for i in data:
             return data[i]["AT"]['av']
 
+def earth_photo():
+    today_date = strftime("%Y-%m-%d", gmtime())
+    year = today_date[:4]
+    month = today_date[5:7]
+    day = today_date[8:10]
+    epic_photos = api.epic(date = today_date)
+    photo_name = epic_photos[0]['image']
+    url_to_image = f"https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/jpg/{photo_name}.jpg"
+    return url_to_image
 #Tabs – 4 хватит
