acc5c1a89fed32e5983f07e2af72546bfa074b51
Shrainer <nikita@shrainer.me>
2020-10-06 17:38:23 +0300

    f

--- nasa_api.py
+++ nasa_api.py
@@ -1,6 +1,7 @@
-import nasapy
+import nasapy, requests
 from time import strftime, gmtime
-api = nasapy.Nasa(key = 'key')
+api_key = 'key'
+api = nasapy.Nasa(key = api_key)
 
 def get_media():
     media = api.picture_of_the_day()
@@ -20,4 +21,12 @@ def earth_photo():
     photo_name = epic_photos[0]['image']
     url_to_image = f"https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/jpg/{photo_name}.jpg"
     return url_to_image
+
+def rover_photo():
+    today_date = strftime("%Y-%m-%d", gmtime())
+    url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={today_date}&api_key={api_key}"
+    result = requests.get(url).text
+    for dictionary in result['photos']:
+        yield dictionary['img_src']
+
 #Tabs – 4 хватит
