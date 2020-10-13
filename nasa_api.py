d7ae7a34f560aef01543d7c127ce39e0fb55b290
Shrainer <nikita@shrainer.me>
2020-10-13 16:03:45 +0300

    f

--- nasa_api.py
+++ nasa_api.py
@@ -1,31 +1,34 @@
-import nasapy, requests
-from time import strftime, gmtime
+import nasapy, requests, json
+from time import strftime, gmtime, time
 api_key = 'key'
-api = nasapy.Nasa(key = api_key)
+api = nasapy.Nasa(api_key)
 
 def get_media():
     media = api.picture_of_the_day()
-    return url_to_media = media['url']
+    return media['url']
 
 def get_weather():
     data = api.mars_weather()
-        for i in data:
-            return data[i]["AT"]['av']
+    for i in data:
+        return data[i]["AT"]['av']
 
-def earth_photo():
-    today_date = strftime("%Y-%m-%d", gmtime())
-    year = today_date[:4]
-    month = today_date[5:7]
-    day = today_date[8:10]
-    epic_photos = api.epic(date = today_date)
-    photo_name = epic_photos[0]['image']
-    url_to_image = f"https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/jpg/{photo_name}.jpg"
-    return url_to_image
+def earth_photo(date = strftime("%Y-%m-%d", gmtime())):
+    year = date[:4]
+    month = date[5:7]
+    day = date[8:10]
+    epic_photos = api.epic(date = date)
+    if not(epic_photos):
+        print("Нету нового фото, отправляю вчерашнее")
+        return earth_photo(strftime("%Y-%m-%d", gmtime(time() - 86400)))
+    else:
+        photo_name = epic_photos[0]['image']
+        url_to_image = f"https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/jpg/{photo_name}.jpg"
+        return url_to_image
 
 def rover_photo():
     today_date = strftime("%Y-%m-%d", gmtime())
     url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={today_date}&api_key={api_key}"
-    result = requests.get(url).text
+    result = json.loads(requests.get(url).text)
     for dictionary in result['photos']:
         yield dictionary['img_src']
 
