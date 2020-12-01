d0f208a75d6d44adebfbb8bfc2e68dbcc6f6aa49
Shrainer <nikita@shrainer.me>
2020-12-01 16:21:25 +0300

    f

---
+++ database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect("mydatabase.db")
+cursor = conn.cursor()
+
+
+def creature():
+    cursor.execute("""CREATE TABLE UsersZXC
+                (user_id integer, user_name text, level integer)
+                """)
+    cursor.execute("""CREATE TABLE MediaZXC
+                (media_name text, media_url text)
+                """)
+    conn.commit()
+
+
+def add_user(id, name, level):
+    cursor.execute(f"""INSERT INTO UsersZXC (user_id, user_name, level)
+                  VALUES ({id}, '{name}', {level}
+                  )""")
+    conn.commit()
+
+def add_media(name, url):
+    cursor.execute(f"""INSERT INTO MediaZXC (media_name, media_url)
+                  VALUES ({name}, '{url}'
+                  )""")
+    conn.commit()
+
+
+add_info()
